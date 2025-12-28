34dc4fb6ec83b592745a4c78ef7e108558bbb25e
Hafiz Muhammad Rashid <rashid.liaquat68@gmail.com>
2025-12-29 01:00:52 +0500

    list

--- test.py
+++ test.py
@@ -1,1 +1,18 @@
 print("Rashid Liaquat");
+
+# Variables
+# name = "Rashid Liaquat"
+# age = 30
+# print(f"Name: {name}, Age: {age}")
+# # Data Types
+# is_student = False  
+# height = 5.9  
+
+
+# Lists
+
+data = [1, 2, 3, 4, 5,'Rashid Liaquat']
+
+print("Data List:", data[1])
+print("Data List:", data[-1])
+print("Data List Length:", len(data))
