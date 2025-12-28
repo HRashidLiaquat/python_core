db991f23e79d98933fc37a30a0188e12209b770e
Hafiz Muhammad Rashid <rashid.liaquat68@gmail.com>
2025-12-29 01:09:06 +0500

    tuple

--- test.py
+++ test.py
@@ -16,3 +16,13 @@ data = [1, 2, 3, 4, 5,'Rashid Liaquat']
 print("Data List:", data[1])
 print("Data List:", data[-1])
 print("Data List Length:", len(data))
+
+
+
+#Tuples
+
+data1= (10, 20, 30, 40, 50,'Rashid Liaquat')
+print("Data1 Tuple:", data1[2])
+print("Data1 Tuple Length:", len(data1))
+print("Data1 Tuple:", data1[-1])
+
