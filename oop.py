563ed97a23a60ef2e2316112aa0b158adcbf48de
Hafiz Muhammad Rashid <rashid.liaquat68@gmail.com>
2026-01-01 20:56:27 +0500

    single inheritance in python

--- oop.py
+++ oop.py
@@ -25,30 +25,59 @@
 # print(realme.show_mode)
 
 
-class Army:
-    def __init__(self):
-        self.name= 'Rashid'
-        self.gn = self.Gun()
+# class Army:
+#     def __init__(self):
+#         self.name= 'Rashid'
+#         self.gn = self.Gun()
 
+#     def show(self):
+#         print("Name: ", self.name)
+#     class Gun:
+#         def __init__(self):
+#             self.name = 'AK47'
+#             self.capacity = '75 Rounds'
+#             self.length = '34.3 In'
+#         def disp(self):
+#             print("Gun name:", self.name)
+#             print("Capacity :", self.capacity)
+#             print("Lenghth :", self.length)
+
+
+# a = Army()
+# print(a.name)
+# a.show()
+# g = a.gn
+
+# print(g.name)
+# print(g.capacity)
+# print(g.length)
+
+
+
+
+#Single Inheritance 
+class Father:
+    money = 1000
     def show(self):
-        print("Name: ", self.name)
-    class Gun:
-        def __init__(self):
-            self.name = 'AK47'
-            self.capacity = '75 Rounds'
-            self.length = '34.3 In'
-        def disp(self):
-            print("Gun name:", self.name)
-            print("Capacity :", self.capacity)
-            print("Lenghth :", self.length)
+        print("Parent class Instance Method")
 
+    @classmethod
 
-a = Army()
-print(a.name)
-a.show()
-g = a.gn
+    def showmoney(cls):
+        print("Parent class class Method:", cls.money)
 
-print(g.name)
-print(g.capacity)
-print(g.length)
+    @staticmethod
+    def stat():
+        a = 10
+        print("Parent class Static Method: ", a)
+class Son(Father):
+    def disp(self):
+        print("Child class Instance Method")
+
+s = Son()
+s.disp()
+s.show()
+s.showmoney()
+s.stat()
+s.show()
 
