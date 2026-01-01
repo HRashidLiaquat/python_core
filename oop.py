47c8c6a6482776897afdb73e9c6e7cad800a17a4
Hafiz Muhammad Rashid <rashid.liaquat68@gmail.com>
2026-01-01 06:49:40 +0500

    class in python

--- oop.py
+++ oop.py
@@ -1,12 +1,25 @@
 class Mobile:
-    def __init__(self):
-        self.model = 'New Mobile'
+    def __init__(self, m):
+        self.model = m
 
-    def show_model(self):
-        self.price = 1000
+    def show_model(self, p):
+        self.price = p
         print("Model:", self.model, "Price", self.price)
-realme = Mobile()
-realme.show_model()
+realme = Mobile('RealMe X')
+realme.show_model(10000)
+print(id(realme))
+print()
+
+redme = Mobile('Redmi 7s')
+redme.show_model(3000)
+print(id(redme))
+print()
+
+
+newmobile = Mobile('New Mobile')
+newmobile.show_model(50000)
+print(id(newmobile))
+
 
 # print(realme.price)
 # print(realme.show_mode)
